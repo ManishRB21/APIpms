5323f042df7f07525aa89d25688b99b0d312cd67
Manish Ranjan Behera <88574302+ManishRB21@users.noreply.github.com>
2024-03-20 22:27:15 +0530

    Update folioRoute.js

--- api/folioRoute.js
+++ api/folioRoute.js
@@ -34,20 +34,48 @@ const folio = require('../folio');
 router.get('/:guestId', (req,res)=>{
   console.log("inide folio")
   const response={
-    data :{
-      id : "101-1",
-      status:"open",
-      balance:65.19,
-      items: [
+  //   data :{
+  //     id : "101-1",
+  //     status:"open",
+  //     balance:65.19,
+  //     items: [
+  //     {
+  //     id: 1,
+  //     created:"2012-05-25T17:27Z",
+  //     description:"in room movie",
+  //     amount:5.19,
+  //     display: true
+  //     }
+  //   ]
+  // }
       {
-      id: 1,
-      created:"2012-05-25T17:27Z",
-      description:"in room movie",
-      amount:5.19,
-      display: true
-      }
-    ]
-  }
+   "status":"success",
+   "data":{
+      "id":"1401-1",
+      "status":"open",
+      "items":[
+         {
+            "auth":{
+               "client_id":"0",
+               "client_secret":"hoteltech"
+            },
+            "amount":1,
+            "description":"test",
+            "purchase_id":"ARAAnQAA",
+            "revenue_code":"1",
+            "subtotal":{
+               "1":1
+            },
+            "tax":{
+               "1":0.12
+            },
+            "id":1,
+            "created":"2024-03-20T09:58:38.432Z"
+         }
+      ],
+      "balance":1
+   }
+}
 }
   res.json(response);
 });
